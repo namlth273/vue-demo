3a3536fe13a131ed0ae4b9e1502fc956ba877d57
Hai nam LE TRINH <hai-nam.letrinh@bollore.com>
2019-01-21 10:10:42 +0700

    commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <AppNavBar/>
+    <router-view/>
+    <AppFooter/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import AppFooter from "@/components/AppFooter.vue";
+import AppNavBar from "@/components/AppNavBar.vue";
+
+@Component({
+  components: {
+    AppFooter, AppNavBar
+  }
+})
+export default class App extends Vue {
+
+}
+</script>
---
+++ src/router.ts
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import ("@/views/Login.vue")
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import ("@/views/Register.vue")
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import ("@/views/Profile.vue")
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: () => import ("@/views/Settings.vue")
+    },
+    {
+      path: '/article',
+      name: 'article',
+      component: () => import ("@/views/Article.vue")
+    },
+    {
+      path: '/editor',
+      name: 'editor',
+      component: () => import ("@/views/Editor.vue")
+    },
+  ],
+})
